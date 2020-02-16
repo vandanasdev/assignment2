d0230ba3300af846895d864b122f0931ad30c191
vandanasdev <60491551+vandanasdev@users.noreply.github.com>
2020-02-15 17:50:21 -0800

    Initial commits

--- public/App.js
+++ public/App.js
@@ -1,16 +1,110 @@
-const element = React.createElement("div", {
-  className: "center"
-}, React.createElement("div", {
-  className: "name"
-}, "VANDANA SINGH"), React.createElement("img", {
-  className: "image",
-  src: "/images/vandanaProfilePic.png"
-}), React.createElement("div", {
-  className: "intro"
-}, " I am pursuing my Master's in Computer Science at SDSU. In the past, I have worked as Quality Assurance Engineer at HiveMedia Group, Carlsbad. I am planning to get into Full Stack Web developer role after I complete my Masters."), React.createElement("div", {
-  className: "button-div"
-}, React.createElement("a", {
-  className: "button",
-  href: "https://github.com/vandanasdev/assignment1"
-}, "VIEW MY GITHUB REPO")));
-ReactDOM.render(element, document.getElementById('myInfo'));
+class IssueFilter extends React.Component {
+  render() {
+    return React.createElement("div", null, " This is a placeholder for Issue Filter");
+  }
+
+}
+
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    console.log("Hi");
+    return React.createElement("tr", null, React.createElement("td", null, issue.id), React.createElement("td", null, issue.status), React.createElement("td", null, issue.owner), React.createElement("td", null, issue.created.toDateString()), React.createElement("td", null, issue.effort), React.createElement("td", null, issue.due ? issue.due.toDateString() : ''), React.createElement("td", null, issue.title));
+  }
+
+}
+
+class IssueTable extends React.Component {
+  render() {
+    const issueRows = this.props.issues.map(issue => React.createElement(IssueRow, {
+      key: issue.id,
+      issue: issue
+    }));
+    return React.createElement("table", {
+      className: "bordered-table"
+    }, React.createElement("thead", null, React.createElement("tr", null, React.createElement("th", null, "ID"), React.createElement("th", null, "Status"), React.createElement("th", null, "Owner"), React.createElement("th", null, "Created"), React.createElement("th", null, "Effort"), React.createElement("th", null, "Due Date"), React.createElement("th", null, "Title"))), React.createElement("tbody", null, issueRows));
+  }
+
+}
+
+class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    const issue = {
+      owner: form.owner.value,
+      title: form.title.value,
+      status: 'New'
+    };
+    this.props.createIssue(issue);
+    form.owner.value = "";
+    form.title.value = "";
+  }
+
+  render() {
+    return React.createElement("form", {
+      name: "issueAdd",
+      onSubmit: this.handleSubmit
+    }, React.createElement("input", {
+      type: "text",
+      name: "owner",
+      placeholder: "Owner "
+    }), React.createElement("input", {
+      type: "text",
+      name: "title",
+      placeholder: "Title"
+    }), React.createElement("button", {
+      type: "submit"
+    }, " Add "));
+  }
+
+}
+
+class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      issues: []
+    };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  createIssue(issue) {
+    issue.id = this.state.issues.length + 1;
+    issue.created = new Date();
+    const newIssueList = this.state.issues.slice();
+    newIssueList.push(issue);
+    this.setState({
+      issues: newIssueList
+    });
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({
+        issues: initialIssues
+      });
+    }, 500);
+  }
+
+  render() {
+    return React.createElement(React.Fragment, null, React.createElement("h1", null, "Issue Tracker"), React.createElement(IssueFilter, null), React.createElement("hr", null), React.createElement(IssueTable, {
+      issues: this.state.issues
+    }), React.createElement("hr", null), React.createElement(IssueAdd, {
+      createIssue: this.createIssue
+    }));
+  }
+
+}
+
+const element = React.createElement(IssueList, null);
+ReactDOM.render(element, document.getElementById('myCompInventory'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,147 @@
-const element = (
-    <div className="center">
-        <div className="name">VANDANA SINGH</div>
-        <img className="image" src='/images/vandanaProfilePic.png' />
-        <div className="intro"> I am pursuing my Master's in Computer Science at SDSU. 
-                    In the past, I have worked as Quality Assurance Engineer at HiveMedia Group, Carlsbad.
-                    I am planning to get into Full Stack Web developer role after I complete my Masters.
-        </div>
-        <div className="button-div">
-            <a className="button" href="https://github.com/vandanasdev/assignment1">VIEW MY GITHUB REPO</a>
-        </div>
+class IssueFilter extends React.Component
+{
+    render()
+    {
+        return(
+            <div> This is a placeholder for Issue Filter</div>
+        );
         
-    </div>
-);
+    }
+}
 
-ReactDOM.render(element, document.getElementById('myInfo'));
+class IssueRow extends React.Component
+{
+    render(){
+        const issue= this.props.issue;
+        
+        console.log("Hi");
+        return(
+            <tr>
+               <td>{issue.id}</td>
+               <td>{issue.status}</td>
+               <td>{issue.owner}</td>
+               <td>{issue.created.toDateString()}</td>
+               <td>{issue.effort}</td>
+               <td>{issue.due ? issue.due.toDateString() : ''}</td>
+               <td>{issue.title}</td>
+            </tr>
+            
+            
+
+        );
+
+    }
+}
+
+class IssueTable extends React.Component
+{
+  
+    render()
+    {
+
+        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+
+        return(
+            <table className="bordered-table" >
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Due Date</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {issueRows} 
+                </tbody>
+            
+            </table>
+        );
+        
+    }
+}
+
+class IssueAdd extends React.Component
+{
+    constructor()
+    {
+        super();
+        this.handleSubmit=this.handleSubmit.bind(this);
+        
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        const form= document.forms.issueAdd;
+        const issue={
+            owner:form.owner.value, title: form.title.value, status: 'New'
+        }
+        this.props.createIssue(issue);
+        form.owner.value=""; form.title.value="";
+    }
+    render()
+    {
+        return(
+            <form name="issueAdd" onSubmit={this.handleSubmit}>
+                <input type="text" name="owner" placeholder="Owner " />
+                <input type="text" name="title" placeholder="Title" />
+                <button type="submit" > Add </button>
+            </form>
+        );
+       
+    }
+}
+
+
+
+class IssueList extends React.Component
+{
+    constructor()
+    {
+        super();
+        this.state= {issues: []};
+        this.createIssue= this.createIssue.bind(this);
+    }
+
+    createIssue(issue){
+        issue.id= this.state.issues.length + 1;
+        issue.created = new Date();
+        const newIssueList = this.state.issues.slice();
+        newIssueList.push(issue);
+        this.setState({issues: newIssueList});
+    }
+
+    componentDidMount()
+    {
+        this.loadData();
+    }
+    
+    loadData()
+    {
+        setTimeout(() => { 
+          this.setState({issues : initialIssues})  
+        }, 500);
+    }
+
+    render(){
+
+        return(
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable issues={this.state.issues}/>
+                <hr />
+                <IssueAdd createIssue={this.createIssue}/>
+            </React.Fragment>
+        );
+
+    }
+}
+
+const element = <IssueList />;
+
+ReactDOM.render(element, document.getElementById('myCompInventory'));
